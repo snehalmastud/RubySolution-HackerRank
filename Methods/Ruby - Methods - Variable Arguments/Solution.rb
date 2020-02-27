158a0c46121d8053a9ff15ba3f277f6cf52d3549
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-27 16:49:28 +0530

    Methods!

---
+++ Methods/Ruby - Methods - Variable Arguments/Solution.rb
@@ -0,0 +1,13 @@
+# Your code here
+def full_name(first_name, *middle_names, last_name)
+    full_name = ''
+    full_name += first_name
+    middle_names.each do |name|
+       full_name += " "
+       full_name += name
+    end
+    
+    full_name += " "
+    full_name += last_name
+    
+end 
