9b938f0d54dc0554809de1c7cec06ba02b6fb45c
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-27 16:42:06 +0530

    Enumerables!

---
+++ Enumerables/Ruby - Enumerable - Introduction/Solution.rb
@@ -0,0 +1,9 @@
+def iterate_colors(colors)
+  # Your code here
+    colors_array = Array.new()
+    colors.each do |color|
+       colors_array << color 
+    end  
+    
+    return colors_array
+end
